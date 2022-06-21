193c78dec1a0bed3d094ed52fad9588d203a61f9
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-06-20 23:05:52 -0400

    small update

---
+++ js/notes.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>现在是 {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    return element
+}
